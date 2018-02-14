4cc23e5740751c2d0647249ff20d5286b9f5bac3
Blagojce Pishmanovski <blagojce.pishmanovski@gmail.com>
2018-02-14 13:16:25 +0100

    Adding class Person

---
+++ class_person.php
@@ -0,0 +1,22 @@
+<?php
+
+class Person 
+{
+	public $name;
+	public $age;
+
+	public function __construct($name)
+	{
+		$this->name = $name;
+	}
+
+	public function setAge($age)
+	{
+		$this->age = $age;
+	}
+}
+
+
+$john = new Person('John Doe');
+$john->setAge(30);
+var_dump($john);
