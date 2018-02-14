17f5ad31601c6844926d33cce82ee943a8d2c5b1
Blagojce Pishmanovski <blagojce.pishmanovski@gmail.com>
2018-02-14 13:28:47 +0100

    Age checking

--- class_person.php
+++ class_person.php
@@ -12,11 +12,16 @@ class Person
 
 	public function setAge($age)
 	{
+		if($age<18)
+		{
+			throw new Exception("Person is not old enough.");
+			
+		}
 		$this->age = $age;
 	}
 }
 
 
 $john = new Person('John Doe');
-$john->setAge(30);
+$john->setAge(17);
 var_dump($john);
