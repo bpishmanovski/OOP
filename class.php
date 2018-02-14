ed2238b595405812b9d0e22ec84ce534907e0f8b
Blagojce Pishmanovski <blagojce.pishmanovski@gmail.com>
2018-02-14 12:36:56 +0100

    OOP

---
+++ class.php
@@ -0,0 +1,23 @@
+<?php
+
+class Task
+{
+	public $title;
+	public $description;
+	public $completed = false;
+
+	public function __construct($title, $description)
+	{
+		$this->title = $title;
+		$this->description = $description;
+	}
+
+	public function complete()
+	{
+		$this->completed = true;
+	}
+
+
+}
+
+$task = new Task('Learn OOP', 'The teacher bla bla...');
